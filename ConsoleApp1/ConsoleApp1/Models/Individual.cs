5b2b113f4eb0a0f4ef06779915a8ea730722d63b
Matthias Mueller <matthiasm@live.de>
2019-03-11 21:25:06 +0100

    Init

---
+++ ConsoleApp1/ConsoleApp1/Models/Individual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1.Models
+{
+public class Individual
+{
+    public List<Address> Addresses { get; set; }
+    public DateTime Birthdate { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Address
+{
+    public string City { get; set; }
+    public IReadOnlyCollection<Street> Streets { get; set; }
+    public int Zip { get; set; }
+}
+
+public class Street
+{
+    public bool IsMainStreet { get; set; }
+    public string StreetName { get; set; }
+    public int StreetNumber { get; set; }
+}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var address1 = new Address
+            {
+                City = "Fake City",
+                Zip = 1234,
+                Streets = new List<Street>
+                {
+                    new Street
+                    {
+                       StreetName = "Grubbenmatte",
+                        StreetNumber = 11,
+                        IsMainStreet = true
+                    },
+                    new Street
+                    {
+                        StreetName = "Bonnetsacher",
+                        StreetNumber = 22,
+                        IsMainStreet = false
+                    },
+                    new Street
+                    {
+                        StreetName = "Hauptbahnhof",
+                        StreetNumber = 33,
+                        IsMainStreet = false
+                    }
+                }
+            };
+
+            var address2 = new Address
+            {
+                City = "Alterswil",
+                Zip = 1715,
+                Streets = new List<Street>
+                {
+                    new Street
+                    {
+                        StreetName = "Baker Street",
+                        StreetNumber = 221,
+                        IsMainStreet = true
+                    },
+                    new Street
+                    {
+                        StreetName = "Greenfield",
+                        StreetNumber = 24,
+                        IsMainStreet = false
+                    }
+                }
+            };
+
+            var individual = new Individual
+            {
+                Addresses = new List<Address> { address1, address2 },
+                Birthdate = new DateTime(1986, 12, 29),
+                FirstName = "Matthias",
+                LastName = "Müller"
+            };
+
+            var serializer = new YamlDotNet.Serialization.Serializer();
+            var yaml = serializer.Serialize(individual);
+            Console.WriteLine(yaml);
+
+            Console.ReadKey();
+        }
+    }
+}
